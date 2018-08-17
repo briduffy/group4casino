615901b4eccbf498f1d171a4ec68d6dd8b5604d2
tchoutdara <monkeymilitia444@gmail.com>
2018-08-17 17:06:29 -0600

    minor user select change

--- blackjack.rb
+++ blackjack.rb
@@ -60,7 +60,7 @@ class Deck
     #{random_card.rank} of #{random_card.suit} (#{random_card.color})
     #{random_card2.rank} of #{random_card2.suit} (#{random_card2.color})"
     user_face_card1 = (random_card.rank == 'J' || random_card.rank == 'Q' || random_card.rank == 'K') ? 10 : random_card.rank
-    @user_card1= (user_face_card1 == 'A') ? 1 : user_face_card1
+    @user_card1 = (user_face_card1 == 'A') ? 1 : user_face_card1
     user_face_card2 = (random_card2.rank == 'J' || random_card2.rank == 'Q' || random_card2.rank == 'K') ? 10 : random_card2.rank
     @user_card2 = (user_face_card2 == 'A') ? 1 : user_face_card2
     if @user_card1.to_i != @user_card2.to_i
@@ -73,16 +73,32 @@ class Deck
     user_selection
   end
 
+  def user_hit
+    hit_card = @shuffle.sample
+    user_hit1 = (hit_card.rank == 'J' || hit.rank == 'Q' || hit_card.rank == 'K') ? 10 : hit_card.rank
+    @user_hit_card = (user_face_card1 == 'A') ? 1 : user_hit1
+    if @user_hit_card.to_i != @user_card1 && @user_card2 && @dealer_card1 && @dealer_card2
+        puts @user_hit_card
+    else
+        user_hit
+    end
+  end
+
   def user_selection
     puts 'What would you like to do with your hand?
         1) Hit
         2) Stay
-        3) Split
-        4) Double Down'
+        3) Exit'
+        # Add split and double down after confirming the rules.
+        # 3) Split
+        # 4) Double Down
     choice = gets.to_i
     case choice
     when 1
         puts "You hit"
+        #user_hit
+    when 3
+        exit
     end
 end
 
