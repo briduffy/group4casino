85381c2b5d9fda6d44060df8cd90baa3f08f27a8
Dirk Dobson <dirkvancouver@live.com>
2018-08-19 21:09:52 -0600

    add dirkgames to menu

--- mainmenu.rb
+++ mainmenu.rb
@@ -5,7 +5,9 @@ def mainmenu
     puts 'Please select a game to play.
     1) Slot Machine
     2) Blackjack
-    3) Exit'
+    3) Lottery
+    4) Exit'
+    
     choice = gets.to_i
     case choice
     when 1
@@ -13,7 +15,10 @@ def mainmenu
     when 2
         Deck.new
     when 3
-        exit
+        require_relative 'dirksgame'
+        DirksGame.new
+    when 4
+        exit   
     else
         puts 'Invalid Choice'
         mainmenu
